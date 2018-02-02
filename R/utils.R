111bdd364e4f9810c481715c6cfe67acee2200e3
katokohaku <kato.satoshi.0@gmail.com>
2018-02-03 01:15:21 +0900

    add: utils::dataSplit

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,11 @@
 #' Same function as 'catf()' in BBmisc except for returning [str]
 #'
-#' @param ...     See sprintf
+#' @param ...     See \code{\link{sprintf}}
 #' @param file    See cat. Default is ""
 #' @param append  See cat. Default is FALSE.
 #' @param newline Append newline at the end? Default is TRUE.
+#'
+#' @return a string
 #'
 #' @export
 
@@ -25,8 +27,8 @@ do.nothing <- function(...){}
 
 #' Remove the First or Last n of an Object
 #'
-#' @param X     a data.frame or a matrix
-#' @param n     an integer. number of row to be removed from last. Same as \code{ head(X, NROW(X) - n)}.
+#' @param X  a data.frame or a matrix
+#' @param n  an integer. number of row to be removed from last. Same as \code{ head(X, NROW(X) - n)}.
 #'
 #' @examples
 #' chop(iris, 140)
@@ -40,6 +42,31 @@ chop <- function(X, n = 1) {
     utils::tail(X, NROW(X) - abs(n))
   }
 }
+
+#' Split dataset into train set and test set (for hold-out)
+#'
+#' @param dataset  a data.frame or a matrix
+#' @param test.ratio  a numelic. Ratio of test data, must be (0, 1).
+#'
+#' @examples
+#' x <- dataSplit(1:10, test.ratio = 0.3)
+#' x <- dataSplit(mtcars, test.ratio = 0.3)
+#'
+#' @export
+
+dataSplit <- function(dataset, test.ratio = 0.1) {
+  stopifnot(!missing(dataset), test.ratio > 0, test.ratio < 1)
+
+  n.test <- floor(NROW(dataset) * test.ratio)
+  split_data <- list(train = chop(dataset, n.test),
+                     test  = tail(dataset, n.test))
+
+  catf("[Split data] train : test = %i : %i obs. (%i colmns)",
+       NROW(split_data$train), NROW(split_data$test), NCOL(dataset))
+  invisible(split_data)
+}
+
+
 #' Scale a matrix with parameters of a scaled matix
 #' @details  X is scaled by \code{center = attr(scaled, "scaled:center")} and \code{scale = attr(scaled, "scaled:scale")}
 #'
