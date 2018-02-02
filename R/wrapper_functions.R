b5277d69d0e8ff9319f1ec06fde35cce4f2f817a
katokohaku <kato.satoshi.0@gmail.com>
2018-02-03 02:53:10 +0900

    add: wrapper functions. predict & plot + rename: buildInstance to learnModel

--- R/wrapper_functions.R
+++ R/wrapper_functions.R
@@ -1,30 +1,15 @@
-#' Plot statistics for suggestion of tweaked instances (population)
+#' TBD
 #'
-#' @param X.train ,
-#' @param true.y ,
-#' @param ntree ,
-#' @param epsiron ,
-#' @param type what type of plot should be drawn. Possible types are
-#' \itemize{
-#'  \item{"randomForest"}{ set ensemble trees to \code{\link{randomForest}}.}
-#'  \item{"xgboost"}{ set ensemble trees to \code{\link{xgboost}}. }
-#' }
-#' \code{"randomForest"} is defult.
-#' @param .tryModel  a logical. If \code{.tryModel = TRUE}, Only build learner.
-#'
-#' @return a list of
-#' \itemize{
-#'  \item{"origin"}{ text. }
-#'  \item{"scaled"}{ text. }
-#'  \item{"forest"}{ text. }
-#'  \item{"rules"}{ text. }
-#'  \item{"esatisfy"}{ text. }
-#' }
-#' Default is "absoluteSum".
+#' @param X.train  .
+#' @param true.y  .
+#' @param ntree  .
+#' @param epsiron  .
+#' @param type  .
+#' @param .tryModel  .
 #'
 #' @export
 
-buildInstance <- function(
+learnModel <- function(
   X.train, true.y, ntree = 50, epsiron = 0.1,
   type = c("randomForest", "xgboost"), .tryModel = FALSE)
 {
@@ -46,10 +31,66 @@ buildInstance <- function(
   obj <- list(origin = X.train,
               scaled = train.scaled,
               forest = forest,
-              rules = rules,
-              esatisfy= es)
+              esatisfy = es)
 
-  class(obj) <- "buildInstance"
+  class(obj) <- "modelTweakFeature"
   invisible(obj)
 }
 
+
+#' TBD
+#'
+#' @param object .
+#' @param ...  .
+#' @param newdata  .
+#' @param label.from  .
+#' @param label.to  .
+#'
+#' @export
+
+predict.modelTweakFeature <- function(
+  object, ..., newdata, label.from, label.to) {
+
+  test.scaled  <- rescale(newdata, scaled = esInstance$scaled)
+
+  tweaked <- tweak(esrules = esInstance$esatisfy,
+                   forest = esInstance$forest,
+                   newdata= test.scaled,
+                   label.from = label.from, label.to = label.to,
+                   .dopar = TRUE)
+
+  obj <- list(scaled   = tweaked,
+              descaled = descale.tweakedFeature(tweaked, test.scaled),
+              label.from = label.from, label.tol = label.to,
+              target   = which(tweaked$predict == label.from))
+
+  class(obj) <- "featureTweak"
+  invisible(obj)
+}
+
+
+#' TBD
+#'
+#' @param x  .
+#' @param ...  .
+#' @param k  .
+#' @param type  .
+#'
+#' @export
+
+
+plot.featureTweak <- function(x, ..., k = NULL, type = "frequency"){
+  stopifnot(!missing(x))
+  pp <- NULL
+  if( is.numeric(k) && k > 0){
+    # plot suggestion for each instance
+    if(k %in% x$target){
+      pp <- plotSuggest(x$scaled, k = k, ...)
+    } else {
+      .mes <- catf("instance[%i] was not predicted as \"%s\"", k, x$label.from)
+    }
+  } else {
+    pp <- plot(x$scaled, type = type)
+  }
+  invisible(pp)
+}
--- sample/randomForest_spamdata_short.R
+++ sample/randomForest_spamdata_short.R
@@ -1,15 +1,9 @@
 # feature tweaking: sample usage with spam data set -----------------------
 require(tidyverse)
-require(magrittr)
-require(randomForest)
-# devtools::install_github("hoxo-m/pforeach")
-# require(pforeach)
 require(featureTweakR)
 
 rm(list=ls())
 set.seed(777)
-
-
 
 # data preparation -------------------------------------------------------------
 data(spam, package = "kernlab")
@@ -17,42 +11,25 @@ dataset <- sample_frac(spam) %>% dataSplit(test.ratio = 0.1)
 
 important.var <- c("charExclamation", "charDollar", "remove", "free", "capitalAve", "capitalLong", "your", "hp")
 data.train <- dataset$train %>% select(important.var)
-data.test  <- dataset$test  %>% select(important.var)
+true.y     <- dataset$train[ ,ncol(dataset$train)]
 
-true.y <- dataset$train[ ,ncol(dataset$train)]
-
+data.test  <- dataset$test  %>% select(important.var) %>% head(50)
 
 # feature tweaking  -----------------------------------------------
 
-buildInstance(X.train = data.train, true.y = true.y, .tryModel = TRUE)
-es <- buildInstance(X.train = data.train, true.y = true.y, ntree = 22)
+# learn model
+es <- learnModel(X.train = data.train, true.y = true.y, ntree = 22)
+ft <- predict(es, newdata = data.test, label.from = "spam", label.to = "nonspam")
+# ft %>% str(2)
 
 
-newdata = data.test
+plot(ft, k=4)
+# plot(ft, k=4, .nonzero.only = TRUE)
+# plot(ft, k=1)
 
-test.scaled  <- rescale(newdata, scaled = train.scaled)
-tweaked <- tweak(es, forest = forest.rf, newdata= test.scaled,
-                 label.from = "spam", label.to = "nonspam",
-                 .dopar = TRUE)
-
-tweaked %>% str
-tweaked$original- tweaked$suggest
-
-dt <- descale.tweakedFeature(tweaked, test.scaled)
-dt %>% str(1)
-
-# plot suggestion for each instance
-which(tweaked$predict == "spam")
-plotSuggest(tweaked, 4)
-
-plotSuggest(tweaked, 11)
-plotSuggest(tweaked, 15)
-plotSuggest(tweaked, 15, .ordered = TRUE, .nonzero.only = TRUE)
+plot(ft, type = "direction")
+# plot(ft, type = "frequency")
 
 
-# Plot population importances
-pp <- plot(tweaked, type="a")
-pp <- plot(tweaked, type="d")
-pp <- plot(tweaked, type="f")
 
 # end ---------------------------------------------------------------------
